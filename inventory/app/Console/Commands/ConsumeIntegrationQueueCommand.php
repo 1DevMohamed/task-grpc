082cf85dd8af49c8afe6f69b5bc64661bd6aec15
swilam <m.swilam@storeus.com>
2026-09-16 12:03:14 +0300

    feat(integration): add ConsumeIntegrationQueueCommand for processing integration messages

---
+++ inventory/app/Console/Commands/ConsumeIntegrationQueueCommand.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Dispatcher\MessageDispatcher;
+use App\Services\SqsService;
+use Illuminate\Console\Command;
+
+class ConsumeIntegrationQueueCommand extends Command
+{
+    protected $signature = 'consume:integration';
+    protected $description = 'Integration worker';
+
+    public function handle(
+        SqsService $sqs,
+        MessageDispatcher $dispatcher
+    ): void {
+        $this->info('Integration worker started.');
+
+        $sqs->consume(
+            queue: 'integration-inbound',
+            callback: function (array $message) use ($dispatcher) {
+                $this->info("Dispatching: {$message['message_type']} [{$message['message_id']}]");
+                $dispatcher->dispatch($message);
+                $this->info("Done: {$message['message_id']}");
+            }
+        );
+    }
+}
